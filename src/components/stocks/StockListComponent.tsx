dd3c7dae3243fd58d4fef737e8e93f49d72e6cd3
MinJae Kim <skwze@naver.com>
2024-11-18 14:13:36 +0900

    HQL-58 KMJ 재고관리 레이아웃 수정 ( 2024-11-18 ) StockListComponent.tsx 에서 총수량 컬럼 삭제, 발주필요목록 삭제, 실수량 부분 버튼추가( 오름차순, 내림차순 정렬 )

--- src/components/stocks/StockListComponent.tsx
+++ src/components/stocks/StockListComponent.tsx
@@ -1,42 +1,21 @@
-import {useState} from "react";
-import {useNavigate} from "react-router";
 
 
 function StockListComponent() {
-
-    const navigate = useNavigate();
-    const {keyword, setKeyword} = useState("")
-    const {searchType, setSearchType} = useState("a")
-
-    const handleChangeKeyword = (e) => {
-        setKeyword(e.target.value);
-    };
-    const handleChangeSearchType = (e) => {
-        setSearchType(e.target.value);
-    };
-
-    const moveToRead = () => {
-        navigate({
-            pathname: `/stock/read`,
-            // search:`${queryStr}`
-        })
-    }
-
     return (
         <div className="pt-5 pb-5 w-full mx-auto">
             <div className="flex flex-col h-screen gap-2 border px-4 py-2 rounded-2xl bg-white shadow-md">
                 {/* 검색 박스 */}
-                <div className="mx-auto w-full px-4 py-6 bg-white border">
+                <div className="mx-auto w-full px-4 py-6 bg-white border">m 
                     <div className="flex gap-5">
                         {/* 검색조건 선택 */}
-                        <select value={searchType} onChange={handleChangeSearchType}
+                        <select
                                 className="px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500">
                             <option value="a">전체</option>
                             <option value="b">상품코드</option>
                             <option value="c">상품명</option>
                         </select>
                         {/* 키워드 입력 */}
-                        <input type="text" value={keyword} onChange={handleChangeKeyword} placeholder={"키워드를 입력해 주세요"}
+                        <input type="text" placeholder={"키워드를 입력해 주세요"}
                                className="border border-black px-2"/>
                         {/* 검색 버튼 */}
                         <button
@@ -51,69 +30,6 @@ function StockListComponent() {
                         className="px-6 py-2 bg-gray-600 text-sm text-white rounded-lg hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500">
                         발주 신청
                     </button>
-                </div>
-
-                <div>
-                    <span>발주가 필요한 상품</span>
-                </div>
-                <div className="-mx-4 px-4">
-                    <div className="min-w-full leading-normal">
-                        {/* table header */}
-                        <div
-                            className="grid grid-cols-12 h-15 border border-b-0 border-gray-400 bg-gray-100 text-center text-xs font-semibold text-gray-600 uppercase tracking-wider">
-                            <div
-                                className="col-span-2 h-full p-2 flex justify-center items-center border-r border-gray-400">
-                                <span>상품코드</span>
-                            </div>
-                            <div
-                                className="col-span-4 h-full p-2 flex justify-center items-center border-r border-gray-400">
-                                <span>상품명</span>
-                            </div>
-                            <div
-                                className="col-span-4 h-full p-2 flex justify-center items-center border-r border-gray-400">
-                                <span>이벤트명</span>
-                            </div>
-                            <div
-                                className="col-span-1 h-full p-2 flex justify-center items-center border-r border-gray-400">
-                                <span>총 수량</span>
-                            </div>
-                            <div
-                                className="col-span-1 h-full p-2 flex justify-center items-center">
-                                <span>실 수량</span>
-                            </div>
-                        </div>
-                        {/* table body */}
-                        <div className="overflow-y-auto">
-                            <div
-                                // onClick={moveToRead}
-                                className="grid grid-cols-12 border border-b-0 border-gray-400 text-center text-xs text-gray-600 uppercase tracking-wider">
-                                <div
-                                    className="col-span-2 h-full p-2 flex justify-center items-center border-r border-gray-400">
-                                    <span>1234567890</span>
-                                </div>
-                                <div
-                                    className="col-span-4 h-full p-4 flex items-center border-r border-gray-400">
-                                    <span>부기 키링</span>
-                                </div>
-                                <div
-                                    className="col-span-4 h-full p-3 flex items-center border-r border-gray-400">
-                                    <span>이벤트명</span>
-                                </div>
-                                <div
-                                    className="col-span-1 h-full p-2 flex justify-center items-center border-r border-gray-400 text-red-400">
-                                    <span>10</span>
-                                </div>
-                                <div
-                                    className="col-span-1 h-full p-1 flex justify-center items-center text-red-400">
-                                    <span>8</span>
-                                </div>
-                            </div>
-                        </div>
-                        {/* table footer */}
-                        <div
-                            className="grid grid-cols-12 h-15 border-t border-gray-400 text-center text-xs text-gray-600 uppercase tracking-wider">
-                        </div>
-                    </div>
                 </div>
 
                 <div>
@@ -139,12 +55,9 @@ function StockListComponent() {
                                 <span>이벤트명</span>
                             </div>
                             <div
-                                className="col-span-1 h-full p-2 flex justify-center items-center border-r border-gray-400">
-                                <span>총 수량</span>
-                            </div>
-                            <div
-                                className="col-span-1 h-full p-2 flex justify-center items-center">
+                                className="col-span-2 h-full p-2 flex gap-2 justify-center items-center">
                                 <span>실 수량</span>
+                                <button>▼( 오름차순 내림차순 정리 )</button>
                             </div>
                         </div>
                         {/* table body */}
@@ -165,11 +78,7 @@ function StockListComponent() {
                                     <span>이벤트명</span>
                                 </div>
                                 <div
-                                    className="col-span-1 h-full p-2 flex justify-center items-center border-r border-gray-400">
-                                    <span>10</span>
-                                </div>
-                                <div
-                                    className="col-span-1 h-full p-1 flex justify-center items-center ">
+                                    className="col-span-2 h-full p-1 flex justify-center items-center ">
                                     <span>8</span>
                                 </div>
                             </div>
